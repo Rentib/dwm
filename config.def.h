5d1f4f7b039baea8af940c671d2916c9a75fe1b8
Rentib <sbitner420@tutanota.com>
2022-10-08 19:21:11 +0200

    port to version 6.4

--- config.def.h
+++ config.def.h
@@ -82,11 +82,10 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
 
-static Key keys[] = {
+static const Key keys[] = {
   /* modifier                     key           function           argument */
   TAGKEYS(                        XK_1,                             0) // TAGKEYS for tab 1
   TAGKEYS(                        XK_2,                             1) // TAGKEYS for tab 2
@@ -216,7 +215,7 @@ static Key keys[] = {
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
-static Button buttons[] = {
+static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
